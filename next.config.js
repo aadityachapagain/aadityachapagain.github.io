2b34e3aaf26e6ecdd171de99ccfeeb9af27f790f
aadityachapagain <chapagain.aaditya@gmail.com>
2023-01-26 13:48:00 +1100

    add next js configuration file

---
+++ next.config.js
@@ -0,0 +1,22 @@
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'aadiimages.imgix.net',
+  },
+}
