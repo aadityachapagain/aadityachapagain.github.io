a98e880e4e56ff5334cc8cf13d5e1d8bdec1c588
aadityachapagain <chapagain.aaditya@gmail.com>
2023-03-08 19:19:08 +1100

    few fixes

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -4,9 +4,7 @@ const resumePage: React.FC = ({}) => {
   return (
     <Fragment>
       <div className="container max-w-4xl m-auto overflow-scroll font-serif px-10 ">
-        <div className="text-2xl ">
-          AADITYA CHAPAGAIN
-        </div>
+        <div className="text-2xl ">AADITYA CHAPAGAIN</div>
       </div>
     </Fragment>
   );
